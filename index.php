d8378f754b5cbab8afbbd7481f763bb4fe60a531
Ronny García Zambrano <86664145+rsgarcia0203@users.noreply.github.com>
2021-12-21 10:28:05 -0500

    Update index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php require(".layout/header.php") ?>
+
+	<main>
+        
+    <meta http-equiv="X-UA-Compatible" content="IE-edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+	
+    <link rel="stylesheet"  href="./css/estilos.css">
+    <title>CURRICULUM VITAE</title>
+</head>
+
+<body>
+    <div class="contenedor">
+        <header>
+            <h3 class = "logo"> Ronny García </h3>
+
+            <nav>
+                <a href="index.php"> Inicio </a>
+                <a href="index.php"> Acerca de </a>
+                <a href="index.php"> Contacto </a>
+            </nav>
+        </header>
+
+        <div class="contenido">
+
+<?php require(".layout/footer.php") ?>
